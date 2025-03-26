e0e8bf9c1295006da7d615de8bbc29ba17cf8b99
Miguel Lúcio <miguelluciosilva@gmail.com>
2025-03-26 14:25:37 -0300

    feat: handle errors

---
+++ src/middlewares/handleAppError.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/AppError.error";
+import { ZodError } from "zod";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+export const handleAppError = (
+  err: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+): any => {
+  if (err instanceof AppError)
+    return res.status(err.statusCode).json({ message: err.message });
+
+  if (err instanceof ZodError)
+    return res.status(400).json({ message: err.flatten().fieldErrors });
+
+  if (err instanceof JsonWebTokenError)
+    return res.status(401).json({ message: err.message });
+
+  return res.status(500).json({ message: "Internal server error." });
+};
