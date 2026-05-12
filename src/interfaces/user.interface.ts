dc6fcb23d2a5f5db7d086d8498728e77e9c636d0
rubenslaurindo <88858333+rubenslaurindo@users.noreply.github.com>
2026-05-11 23:25:25 -0300

    Feat/api integration2 (#27)
    * git push --set-upstream origin feat/api-integration2 (#25)
    
    * Feat/api integration (#21)
    
    * feat: git push --set-upstream origin feat/api-integration2
    
    * feat: git push --set-upstream origin feat/api-integration3
    
    * feat: git push --set-upstream origin feat/api-integration4
    
    ---------
    
    Co-authored-by: projeto-integrador-comp <projeto.integrador.comp@gmail.com>
    
    * Feat/api integration (#22)
    
    * feat: git push --set-upstream origin feat/api-integration2
    
    * feat: git push --set-upstream origin feat/api-integration3
    
    * feat: git push --set-upstream origin feat/api-integration4
    
    ---------
    
    Co-authored-by: projeto-integrador-comp <projeto.integrador.comp@gmail.com>
    
    * Revert "Feat/api integration (#22)" (#23)
    
    This reverts commit bbb484580639ee9b4db2f2ab06f4ad25b461389e.
    
    * Revert "Feat/api integration (#21)" (#24)
    
    This reverts commit 0cb5a3cf16a36aee0398d259575b215fd62f2a56.
    
    ---------
    
    Co-authored-by: projeto-integrador-comp <projeto.integrador.comp@gmail.com>
    
    * fix: corrige importacao no user.interface quebrando o build
    
    * fix: remove userRoleOmitSchema para corrigir build do render
    
    ---------
    
    Co-authored-by: projeto-integrador-comp <projeto.integrador.comp@gmail.com>

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,6 +1,5 @@
 import { z } from "zod";
 import {
-  // userRoleOmitSchema,
   userCreateSchema,
   userListSchema,
   userReturnSchema,
@@ -10,5 +9,5 @@ import { DeepPartial } from "typeorm";
 export type TUserCreate = z.infer<typeof userCreateSchema>;
 export type TUserReturn = z.infer<typeof userReturnSchema>;
 export type TUserList = z.infer<typeof userListSchema>;
-export type TUserRoleOmit = z.infer<typeof userRoleOmitSchema>;
-export type TUserUpdate = DeepPartial<TUserRoleOmit>;
+
+export type TUserUpdate = DeepPartial<TUserCreate>;
